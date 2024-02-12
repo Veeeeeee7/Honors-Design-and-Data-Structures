b915facff43780c6483f9a3fa3c0821a1d89ec2b
Victor <victorli1076@gmail.com>
2024-02-11 18:12:01 -0800

    remove done?

--- BST/MyBST.java
+++ BST/MyBST.java
@@ -20,15 +20,12 @@ public class MyBST<E extends Comparable<E>> {
 
 	private boolean containsHelper(BinaryNode<E> node, E value) {
 		E currentValue = node.getValue();
-		if (node.getValue().equals(value)) {
+		if (currentValue.equals(value)) {
 			return true;
 		}
-		if (!node.hasChildren()) {
-			return false;
-		}
-		if (value.compareTo(currentValue) > 0) {
+		if (node.hasRight() && value.compareTo(currentValue) > 0) {
 			return containsHelper(node.getRight(), value);
-		} else if (value.compareTo(currentValue) < 0) {
+		} else if (node.hasLeft() && value.compareTo(currentValue) < 0) {
 			return containsHelper(node.getLeft(), value);
 		}
 		return false;
@@ -65,7 +62,120 @@ public class MyBST<E extends Comparable<E>> {
 	// Removes value from this BST. Returns true if value has been
 	// found and removed; otherwise returns false.
 	public boolean remove(E value) {
-		return false;
+		return removeHelper(root, value);
+	}
+
+	private boolean removeHelper(BinaryNode<E> node, E value) {
+		if (root.getValue().equals(value)) {
+			if (root == null) {
+				return false;
+			}
+			if (!root.hasChildren()) {
+				root = null;
+				return true;
+			}
+			if (!root.hasRight()) {
+				root = root.getLeft();
+				return true;
+			}
+			if (!root.hasLeft()) {
+				root = root.getRight();
+				return true;
+			}
+
+			BinaryNode<E> leftMax = getBeforeMax(root.getLeft(), root);
+			// System.out.println(leftMax.getValue());
+			if (leftMax.getValue().equals(root.getValue())) {
+				root.setValue(root.getLeft().getValue());
+				root.setLeft(root.getLeft().getLeft());
+				return true;
+			}
+
+			if (leftMax.hasRight()) {
+				root.setValue(leftMax.getRight().getValue());
+				leftMax.setRight(null);
+			} else if (leftMax.hasLeft()) {
+				root.setValue(leftMax.getLeft().getValue());
+				leftMax.setLeft(null);
+			}
+
+		}
+
+		BinaryNode<E> previousNode = findPreviousNode(root, value);
+		boolean left = false;
+		BinaryNode<E> removeNode = new BinaryNode<>(null);
+		if (previousNode == null) {
+			return false;
+		}
+		if (value.compareTo(previousNode.getValue()) > 0) {
+			removeNode = previousNode.getRight();
+			left = false;
+		} else if (value.compareTo(previousNode.getValue()) < 0) {
+			removeNode = previousNode.getLeft();
+			left = true;
+		}
+
+		if (!removeNode.hasChildren()) {
+			if (left) {
+				previousNode.setLeft(null);
+			} else {
+				previousNode.setRight(null);
+			}
+		}
+
+		if (!removeNode.hasLeft()) {
+			if (left) {
+				previousNode.setLeft(removeNode.getRight());
+			} else {
+				previousNode.setRight(removeNode.getRight());
+			}
+		}
+
+		if (!removeNode.hasRight()) {
+			if (left) {
+				previousNode.setLeft(removeNode.getLeft());
+			} else {
+				previousNode.setRight(removeNode.getLeft());
+			}
+		}
+
+		BinaryNode<E> leftMax = getBeforeMax(removeNode, removeNode);
+
+		if (leftMax.hasRight()) {
+			removeNode.setValue(leftMax.getRight().getValue());
+			leftMax.setRight(null);
+		} else if (leftMax.hasLeft()) {
+			removeNode.setValue(leftMax.getLeft().getValue());
+			leftMax.setLeft(null);
+		}
+
+		return true;
+	}
+
+	private BinaryNode<E> getBeforeMax(BinaryNode<E> node, BinaryNode<E> previousNode) {
+		if (node.hasRight()) {
+			return getBeforeMax(node.getRight(), node);
+		}
+		// if (node.hasLeft()) {
+		// return getBeforeMax(node.getLeft(), node);
+		// }
+		return previousNode;
+	}
+
+	private BinaryNode<E> findPreviousNode(BinaryNode<E> node, E value) {
+		E currentValue = node.getValue();
+		if (node.hasRight() && node.getRight().getValue().equals(value)) {
+			return node;
+		} else if (node.hasLeft() && node.getLeft().getValue().equals(value)) {
+			return node;
+		}
+
+		if (node.hasRight() && value.compareTo(currentValue) > 0) {
+			return findPreviousNode(node.getRight(), value);
+		} else if (node.hasLeft() && value.compareTo(currentValue) < 0) {
+			return findPreviousNode(node.getLeft(), value);
+		}
+		return null;
 	}
 
 	// Returns a bracket-surrounded, comma separated list of the contents of the
--- BST/tester.java
+++ BST/tester.java
@@ -2,10 +2,14 @@ public class tester {
     public static void main(String[] args) {
         MyBST<Integer> tree = new MyBST<>();
         tree.add(5);
-        tree.add(6);
-        tree.add(4);
-        tree.add(3);
         tree.add(7);
-        System.out.println(tree.contains(8));
+        tree.add(6);
+        tree.add(8);
+        tree.add(1);
+        tree.add(0);
+        tree.add(-1);
+        tree.add(2);
+        tree.remove(2);
+        System.out.println(tree);
     }
 }
